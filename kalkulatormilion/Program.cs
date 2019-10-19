af7122a94e9570e453f2fe7e1df9729b1787a4ce
Michal Zylowski <zylowskimichal@gmail.com>
2019-10-19 09:05:37 +0200

    Add menu to application

---
+++ kalkulatormilion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kalkulatormilion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("0. Wyjscie");
+                Console.WriteLine("1. Dodawanie");
+                Console.WriteLine("2. Odejmowanie");
+                int menuOption = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
