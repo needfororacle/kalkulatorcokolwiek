565641a327ef4883e6f96906109e2bb7ef14ff70
Michal Zylowski <zylowskimichal@gmail.com>
2019-10-19 09:47:49 +0200

    Reorganize menu

--- kalkulatormilion/Program.cs
+++ kalkulatormilion/Program.cs
@@ -12,12 +12,12 @@ namespace kalkulatormilion
         {
             while (true)
             {
-                Console.WriteLine("0. Wyjscie");
-                Console.WriteLine("1. Dodawanie");
-                Console.WriteLine("2. Odejmowanie");
                 int menuOption = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("4. Wyjscie");
+                Console.WriteLine("1. ADD");
                 if (menuOption == 0)
                     break;
+                Console.WriteLine("2. DIFF");
             }
         }
     }
